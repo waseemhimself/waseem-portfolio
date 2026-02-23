b963491479c4627439abe68403f583f72e8a2be8
waseemhimself <waseemwhack@gmail.com>
2026-02-23 15:13:43 +0530

    Lazy load model (2)

--- backend/app/rag.py
+++ backend/app/rag.py
@@ -1,21 +1,47 @@
 from sentence_transformers import SentenceTransformer
 from sqlalchemy.orm import Session
 from .models import ResumeChunk
-from pgvector.sqlalchemy import Vector
 import numpy as np
 
-# Load embedding model once
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model
 
 def embed_text(text: str):
-    embedding = model.encode(text)
+    embedding = get_model().encode(text)
     return embedding.tolist()
 
 def get_similar_chunks(db: Session, query: str, k: int = 3):
     query_embedding = embed_text(query)
+    results = db.query(ResumeChunk).order_by(  
+        ResumeChunk.embedding.l2_distance(query_embedding)  
+        ).limit(k).all()  
 
-    results = db.query(ResumeChunk).order_by(
-        ResumeChunk.embedding.l2_distance(query_embedding)
-    ).limit(k).all()
+    return results  
 
-    return results
+
+# from sentence_transformers import SentenceTransformer
+# from sqlalchemy.orm import Session
+# from .models import ResumeChunk
+# from pgvector.sqlalchemy import Vector
+# import numpy as np
+
+# # Load embedding model once
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def embed_text(text: str):
+#     embedding = model.encode(text)
+#     return embedding.tolist()
+
+# def get_similar_chunks(db: Session, query: str, k: int = 3):
+#     query_embedding = embed_text(query)
+
+#     results = db.query(ResumeChunk).order_by(
+#         ResumeChunk.embedding.l2_distance(query_embedding)
+#     ).limit(k).all()
+
+#     return results
